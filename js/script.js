e6a7f1899b93167a60b12470eee3deb683f75ccf
Tal Paschal <talpaschal@gmail.com>
2019-02-05 17:10:45 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,40 +19,40 @@ FSJS project 1 - A Random Quote Generator
 let quotes = [
   {
     quote: "Your time is limited, so don’t waste it living someone else’s life.",
-    source: " –Steve Jobs"
+    source: " Steve Jobs"
   },
   {
     quote: "The only person you are destined to become is the person you decide to be.",
-    source: " –Ralph Waldo Emerson"
+    source: " Ralph Waldo Emerson"
   },
   {
     quote: "Start where you are. Use what you have. Do what you can.",
-    source: " –Arthur Ashe"
+    source: " Arthur Ashe"
   },
   {
     quote: "When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
-    source: " -Hellen Keller"
+    source: " Hellen Keller"
   },
   {
     quote: "Life is not measured by the number of breaths we take, but by the moments that take our breath away.",
-    source: " –Maya Angelou"
+    source: " Maya Angelou"
   },
   {
     quote: "Happiness is not something readymade. It comes from your own actions.",
-    source: " –Dalai Lama"
+    source: " Dalai Lama"
   },
   {
     quote: "Life is not so short but that there is always time enough for courtesy.",
-    source: " -Ralph Waldo Emerson",
+    source: " Ralph Waldo Emerson",
     citation: " 'Letters and Social Aims'",
-    year: " 1876"
+    year: 1876
     
  },
  {
   quote: "I can accept failure. Everyone fails at something. But I can't accept not trying [no hard work].", 
-  source: " -Michael Jordan",
+  source: " Michael Jordan",
   citation: " 'I Can't Accept Not Trying : Michael Jordan on the Pursuit of Excellence'",
-  year: " 1994"
+  year: 1994
   
  }
 
@@ -70,7 +70,7 @@ let quotes = [
 
 
 function getRandomQuote(array) {
-  let randomNumber = Math.floor(Math.random() * array.length) + 1;
+  let randomNumber = Math.floor(Math.random() * array.length);
   return array[randomNumber];
 }
 
@@ -89,10 +89,10 @@ function printQuote () {
   let result = '';
   result += '<p class="quote">' + random.quote + '</p>';
   result += '<p class="source">' + random.source + '</p>';
-  if ( random.citation === true ) {
-    result += '<p><span class="citation">' + random.citation + '</span></p>';
-  } else if (random.year === true) {
-    result += '<p><span class="year">' + random.year + '</span></p>';
+  if ( random.citation === true && random.year === true ) {
+    result += '<p><span class="citation">' + random.citation + '</span>' + '<span class="year">' + random.year + '</span></p>';
+  } else {
+    result += '';
   }
   document.getElementById('quote-box').innerHTML = result;
 
